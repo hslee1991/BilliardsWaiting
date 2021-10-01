bd9d41463e933097c7ac63e48c1d582eedf1998d
hslee_sinsisao <hslee.sinsisao@gmail.com>
2021-10-01 19:12:28 +0900

    insert views to main view control class. rename waiting_counts to waiting_count.

--- app/src/main/java/com/sinsisao/billiardswaiting/Control/MainViewControl.java
+++ app/src/main/java/com/sinsisao/billiardswaiting/Control/MainViewControl.java
@@ -1,4 +1,45 @@
 package com.sinsisao.billiardswaiting.Control;
 
-class MainViewControl {
+import android.view.View;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.sinsisao.billiardswaiting.Model.CustomerData;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainViewControl {
+
+    private View mRootView = null;
+    private ListView mWaitingListView = null;
+    private TextView mWaitingCount = null;
+    private List mCustomerDataList = null;
+
+    public MainViewControl(View a_rootView){
+        mRootView = a_rootView;
+        mCustomerDataList = new ArrayList<CustomerData>();
+    }
+
+    public void setWaitingList(int a_id) {
+        if (mWaitingListView == null) {
+            return;
+        }
+        try {
+            mWaitingListView = (ListView) mRootView.findViewById(a_id);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void setWaitingCount(int a_id) {
+        if (mWaitingCount == null) {
+            return;
+        }
+        try {
+            mWaitingCount = (TextView) mRootView.findViewById(a_id);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
--- app/src/main/java/com/sinsisao/billiardswaiting/View/MainActivity.java
+++ app/src/main/java/com/sinsisao/billiardswaiting/View/MainActivity.java
@@ -3,8 +3,11 @@ package com.sinsisao.billiardswaiting.View;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ListView;
 
 
+import com.sinsisao.billiardswaiting.Control.MainViewControl;
 import com.sinsisao.billiardswaiting.R;
 import com.sinsisao.billiardswaiting.Utils.WaitingLog;
 
@@ -14,23 +17,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        View rootView = (View)findViewById(R.id.root);
+        MainViewControl mvc = new MainViewControl(rootView);
+        mvc.setWaitingList(R.id.waiting_board);
+        mvc.setWaitingCount(R.id.waiting_count);
     }
 
     @Override
     protected void onResume() {
         super.onResume();
         WaitingLog.d("onResume()");
-    }
 
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        WaitingLog.d("onDestroy()");
+
     }
 
     @Override
     protected void onPause() {
         super.onPause();
         WaitingLog.d("onPause()");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        WaitingLog.d("onDestroy()");
     }
 }
